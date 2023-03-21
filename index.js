42738e8df7b72625440cf5e9d51b461e997726ad
MJephat <mukundijephat22@gmail.com>
2023-03-21 14:21:56 +0300

    Assignment is done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById("main").remove();
+//create a new element 
+const newHeader = document.createElement("H1");
+//create an id = victory.
+newHeader.id = 'victory';
+//adding a text.
+newHeader.innerHTML = "Jephat is the champion.";
